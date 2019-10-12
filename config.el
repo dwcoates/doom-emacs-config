ce3b789323ba16dfa6402dad297613b30c044118
Dodge W Coates <dodge@system76-pc.localdomain>
2019-10-11 21:16:12 -0400

    Update bindings to be more emacsian in insert-mode.

--- config.el
+++ config.el
@@ -1,10 +1,19 @@
 ;;; .doom.d/config.el -*- lexical-binding: t; -*-
 
+ ;; Use normal previous/next keys in insert-mode
+(unmap! evil-insert-state-map "C-p" "C-n")
+
 ;; Place your private configuration here
-(map! :nvg
-      "C-k" 'kill-line
-      :nvig
-      "C-e" 'end-of-line)
+(map!
+ ;; Use some basic emacs navigation bindings in insert-mode
+ :nvig
+ "C-k" 'kill-line
+ "C-e" 'end-of-line
+ :map global-map :i
+ "C-d" 'delete-char
+ "C-p" 'previous-line
+ "C-n" 'next-line
+
 
 ;; (after! avy)
 (setq display-line-numbers-type 'relative)
