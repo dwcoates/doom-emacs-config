769af7b4ae240376398363b99653d22b24c2b16f
Dodge W. Coates <dodge.w.coates@gmail.com>
2019-10-16 12:02:15 -0400

    evil-surround bindings.

--- config.el
+++ config.el
@@ -1,18 +1,26 @@
 ;;; .doom.d/config.el -*- lexical-binding: t; -*-
 
  ;; Use normal previous/next keys in insert-mode
-(unmap! evil-insert-state-map "C-p" "C-n")
+(unmap! evil-insert-state-map "C-p" "C-n") ;; No keys stealing C-p and C-n, for now.
+(unmap! evil-visual-state-map "s") ;; No snipe in visual mode
 
 ;; Place your private configuration here
 (map!
  ;; Use some basic emacs navigation bindings in insert-mode
- :nvig
+ :nvigr
  "C-k" 'kill-line
  "C-e" 'end-of-line
- :map global-map :i
+ :map global-map
+ :i
  "C-d" 'delete-char
  "C-p" 'previous-line
  "C-n" 'next-line
+ ;; Surround (paren manipulation)
+ :n
+ "M-]" 'evil-surround-delete
+ "M-[" 'evil-surround-change
+ :v
+ "s"   'evil-surround-edit
 
  ;; File finding. FIXME: This seems like a shitty way to do this.
  :prefix "C-x M-f"
