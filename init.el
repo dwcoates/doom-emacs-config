74dce7dd8b658b40a9a70f6f1914ecefeb4e2832
Dodge W Coates <dodge@blackbox.localdomain>
2019-11-02 18:54:45 -0400

    Add clojure and common lisp core.

--- init.el
+++ init.el
@@ -97,8 +97,8 @@
        ;;agda              ; types of types of types of types...
        ;;assembly          ; assembly for fun or debugging
        cc                  ; C/C++/Obj-C madness
-       ;;clojure           ; java with a lisp
-       ;;common-lisp       ; if you've seen one lisp, you've seen them all
+       clojure           ; java with a lisp
+       common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
        ;;csharp            ; unity, .NET, and mono shenanigans
