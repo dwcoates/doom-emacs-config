043b9155124be5e525bdd8d73265c85accc98201
Dodge W Coates <dodge@blackbox.localdomain>
2019-11-17 19:39:19 -0500

    Always use rtags.

--- modules/lang/personal-cc/config.el
+++ modules/lang/personal-cc/config.el
@@ -3,12 +3,15 @@
 (add-to-list 'exec-path "$HOME/usr/lib/llvm-6.0/lib/clang/6.0.0/include")
 
 (after! rtags
-  (setq rtags-path "/home/dodge/src/rtags/bin"))
+  (setq rtags-path "/home/dodge/src/rtags/bin")
+
+  (defun dwc-rtags-hook ()
+    (rtags-enable-standard-keybindings))
+  (add-hook 'rtags-mode-hook rtags-enable-standard-keybindings)
+
+  (add-hook 'c-mode-hook 'rtags-start-process-unless-running)
+  (add-hook 'c++-mode-hook 'rtags-start-process-unless-running))
 
 ;;; C/C++
 (after! cc-mode
-  (set-pretty-symbols! 'c++-mode nil)
-
-  ;; Rtags
-  (add-hook 'c-mode-hook 'rtags-start-process-unless-running)
-  (add-hook 'c++-mode-hook 'rtags-start-process-unless-running))
+  (set-pretty-symbols! 'c++-mode nil))
