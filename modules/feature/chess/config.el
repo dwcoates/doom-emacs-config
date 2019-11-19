9cb8291021292814c02bd75bd9621b7ccc5f8a80
Dodge W Coates <dodge@blackbox.localdomain>
2019-11-18 21:36:03 -0500

    Switch to pygn mode! Kill chess.

--- modules/feature/chess/config.el
+++ modules/feature/chess/config.el
@@ -1,71 +1,71 @@
 ;;; ~/.doom.d/chess.el -*- lexical-binding: t; -*-
 ;;;
 ;;;
-;;; Configuration and modification file for emacs-chess.
+ ;;; Configuration and modification file for emacs-chess.
 
 
-(use-package! pgn-mode
+(use-package! pygn-mode
   :config
-  (map! :map pgn-mode-map
-        :g "M-h" #'pgn-mode-previous-move
-        :g "M-l" #'pgn-mode-next-move
-        :g "M-j" #'pgn-mode-next-game
-        :g "M-k" #'pgn-mode-previous-game))
+  (map! :map pygn-mode-map
+        :g "M-h" #'pygn-mode-previous-move
+        :g "M-l" #'pygn-mode-next-move
+        :g "M-j" #'pygn-mode-next-game
+        :g "M-k" #'pygn-mode-previous-game))
 
-;;; Chess
-(use-package! chess
-  :commands (+chess-ivy-show-fens +chess-ivy-show-fens-new-frame +chess-make-pos-from-fen)
-  :init
-  (map! :leader
-        (:prefix-map ("k" . "chess")
-          :desc "Display position for FEN found on current line." "f" '+chess-ivy-show-fens
-          :desc "Display position for FEN found on current line in new frame." "F" '+chess-ivy-show-fens-new-frame))
-  :config
-  ;; TODO: this decision should really be made on a screen-size basis.
-  (set-popup-rule! "^\\*Chessboard.*" :side (if (string= system-name "blackbox") 'top 'left))
+;; ;;; Chess
+;; (use-package! chess
+;;   :commands (+chess-ivy-show-fens +chess-ivy-show-fens-new-frame +chess-make-pos-from-fen)
+;;   :init
+;;   (map! :leader
+;;         (:prefix-map ("k" . "chess")
+;;           :desc "Display position for FEN found on current line." "f" '+chess-ivy-show-fens
+;;           :desc "Display position for FEN found on current line in new frame." "F" '+chess-ivy-show-fens-new-frame))
+;;   :config
+;;   ;; TODO: this decision should really be made on a screen-size basis.
+;;   (set-popup-rule! "^\\*Chessboard.*" :side (if (string= system-name "blackbox") 'top 'left))
 
-  (add-to-list 'evil-emacs-state-modes 'chess-display-mode)
+;; (add-to-list 'evil-emacs-state-modes 'chess-display-mode)
 
-  (map! :map chess-display-mode
-        :g
-        "h" 'chess-display-move-forward)
+;; (map! :map chess-display-mode
+;;       :g
+;;       "h" 'chess-display-move-forward)
 
-  (defun +chess-make-pos-from-fen (fen)
-    "Display the position resulting from FEN."
-    (let* ((game (chess-game-create))
-           (new-display (chess-display-create game 'chess-images nil)))
-      (chess-game-set-start-position game (chess-fen-to-pos fen))
-      (chess-display-set-game new-display game 1)
-      (chess-display-popup new-display)))
+;; (defun +chess-make-pos-from-fen (fen)
+;;   "Display the position resulting from FEN."
+;;   (let* ((game (chess-game-create))
+;;          (new-display (chess-display-create game 'chess-images nil)))
+;;     (chess-game-set-start-position game (chess-fen-to-pos fen))
+;;     (chess-display-set-game new-display game 1)
+;;     (chess-display-popup new-display)))
 
-  (setq! chess-images-directory "/home/dodge/.emacs.d/.local/straight/repos/emacs-chess/pieces/xboard"
-         chess-images-default-size 40
-         chess-images-separate-frame nil)
+;; (setq! chess-images-directory "/home/dodge/.emacs.d/.local/straight/repos/emacs-chess/pieces/xboard"
+;;        chess-images-default-size 40
+;;        chess-images-separate-frame nil)
 
-  (defun +chess-images-popup ()
-    "A special chess images popup function to work with doom buffers."
-    (unless chess-images-size
-      (chess-error 'no-images))
+;; (defun +chess-images-popup ()
+;;   "A special chess images popup function to work with doom buffers."
+;;   (unless chess-images-size
+;;     (chess-error 'no-images))
 
-    (let* ((size (float (+ (* (or chess-images-border-width 0) 8)
-                           (* chess-images-size 8))))
-           (max-char-height (ceiling (/ size (frame-char-height))))
-           (max-char-width  (ceiling (/ size (frame-char-width)))))
-      ;; create the frame whenever necessary
-      (if chess-images-separate-frame
-          (chess-display-popup-in-frame
-           (+ max-char-height 2)
-           max-char-width
-           (cdr (assq 'font (frame-parameters))))
-        (+chess--display-popup-in-window nil (+ max-char-height 1)))))
+;;   (let* ((size (float (+ (* (or chess-images-border-width 0) 8)
+;;                          (* chess-images-size 8))))
+;;          (max-char-height (ceiling (/ size (frame-char-height))))
+;;          (max-char-width  (ceiling (/ size (frame-char-width)))))
+;;     ;; create the frame whenever necessary
+;;     (if chess-images-separate-frame
+;;         (chess-display-popup-in-frame
+;;          (+ max-char-height 2)
+;;          max-char-width
+;;          (cdr (assq 'font (frame-parameters))))
+;;       (+chess--display-popup-in-window nil (+ max-char-height 1)))))
 
-  ;; We set the chess-images popup to be doom-compatible.
-  (setq chess-images-popup-function ' +chess-images-popup)
+;; ;; We set the chess-images popup to be doom-compatible.
+;; (setq chess-images-popup-function ' +chess-images-popup)
 
-  (defun +chess--display-popup-in-window (&optional max-h min-h max-w min-w)
-    "Popup the given DISPLAY, so that it's visible to the user."
-    (unless (get-buffer-window (current-buffer))
-      (if (> (length (window-list)) 1)
-          (fit-window-to-buffer (display-buffer (current-buffer))
-                                max-h min-h max-w min-w)
-        (display-buffer (current-buffer))))))
+;; (defun +chess--display-popup-in-window (&optional max-h min-h max-w min-w)
+;;   "Popup the given DISPLAY, so that it's visible to the user."
+;;   (unless (get-buffer-window (current-buffer))
+;;     (if (> (length (window-list)) 1)
+;;         (fit-window-to-buffer (display-buffer (current-buffer))
+;;                               max-h min-h max-w min-w)
+;;       (display-buffer (current-buffer))))))
--- modules/feature/chess/packages.el
+++ modules/feature/chess/packages.el
@@ -1,5 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; chess/packages.el
 
-;; Use my forked version.
-(package! pgn-mode :recipe (:host github :repo "dwcoates/pgn-mode" :upgrade t))
+(package! pygn-mode :recipe (:host github :repo "dwcoates/pygn-mode"))
