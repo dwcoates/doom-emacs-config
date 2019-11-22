87f3fec087bf2ba49ccbae9bf7540d0df41cefb6
Dodge W Coates <dodge@blackbox.localdomain>
2019-11-22 11:32:01 -0500

    Prepend Bugs and Todays.

--- modules/lang/personal-org/config.el
+++ modules/lang/personal-org/config.el
@@ -29,14 +29,14 @@
                                 ("ptt" "Today's todos" entry
                                  (file+headline +org-capture-project-todo-file "Today")
                                  "* TODO %?\n %i\n %a"
-                                 :clock-in t)
+                                 :clock-in t :prepend t)
                                 ("ptf" "Future features" entry
                                  (file+headline +org-capture-project-todo-file "Features"))
                                 ("ptr" "Future refactors" entry
                                  (file+headline +org-capture-project-todo-file "Refactor"))
                                 ("ptb" "Bugs to fix" entry
                                  (file+headline +org-capture-project-todo-file "Bugs")
-                                 "* [ ] %?\t%t\n %i\n %a")
+                                 "* [ ] %?\t%t\n %i\n %a" :prepend t)
                                 ("pn" "Project-local notes" entry ; {project-root}/notes.org
                                  (file+headline +org-capture-project-notes-file "Inbox"))
                                 ("pc" "Project-local changelog" entry ; {project-root}/changelog.org
