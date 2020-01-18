62317a839e41e8ee12198562dfe0d71db60f2c40
Dodge W Coates <dodge@blackbox.localdomain>
2020-01-18 12:30:41 -0500

    Don't kill GDB input popup.

--- modules/lang/personal-cc/config.el
+++ modules/lang/personal-cc/config.el
@@ -13,4 +13,5 @@
 ;;; C/C++
 (after! cc-mode
   (set-pretty-symbols! 'c++-mode nil)
-  (set-popup-rule! "*compilation*" :side 'bottom :height 20 :quit #'doom/popup-ctrl-g-close :select t))
+  (set-popup-rule! "*compilation*" :side 'bottom :height 20 :quit #'doom/popup-ctrl-g-close :select t)
+  (set-popup-rule! "*input/output of .*" :quit :quit #'doom/popup-ctrl-g-close ))
