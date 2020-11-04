504d7d95a73c5fe5fd49def8949d6c3231618bf3
Dodge W. Coates (goldbox) <dodge@chess.com>
2020-11-03 20:33:16 -0500

    Remove pygn mode temporarily.

--- modules/feature/chess/config.el
+++ modules/feature/chess/config.el
@@ -3,20 +3,20 @@
 ;;;
  ;;; Configuration and modification file for emacs-chess.
 
-(load-file "/home/dodge/workspace/pygn-mode/pygn-mode.el")
+;(load-file "/home/dodge/workspace/pygn-mode/pygn-mode.el")
 
-(use-package! pygn-mode
-  :config
-  (map! :map pygn-mode-map
-        :g "h" #'pygn-mode-previous-move
-        :g "H" #'pygn-mode-previous-move-follow-gui-board
-        :g "l" #'pygn-mode-next-move
-        :g "L" #'pygn-mode-next-move-follow-gui-board
-        :g "j" #'pygn-mode-next-game
-        :g "k" #'pygn-mode-previous-game
-        :g "RET" #'pygn-mode-echo-fen-at-point
-        :g "<mouse-8>" #'pygn-mode-mouse-display-gui-board)
-  (set-popup-rule! "*pygn-mode-board*" :ignore t))
+;(use-package! pygn-mode
+;  :config
+;  (map! :map pygn-mode-map
+;        :g "h" #'pygn-mode-previous-move
+;        :g "H" #'pygn-mode-previous-move-follow-gui-board
+;        :g "l" #'pygn-mode-next-move
+;        :g "L" #'pygn-mode-next-move-follow-gui-board
+;        :g "j" #'pygn-mode-next-game
+;        :g "k" #'pygn-mode-previous-game
+;        :g "RET" #'pygn-mode-echo-fen-at-point
+ ;       :g "<mouse-8>" #'pygn-mode-mouse-display-gui-board
+ ; (set-popup-rule! "*pygn-mode-board*" :ignore t)
 
 ;; ;;; Chess
 ;; (use-package! chess
