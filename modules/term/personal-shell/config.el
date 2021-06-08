65a113a16dd38ca33afa90ccfb1c6170403198f5
Dodge Coates <dodge@goldbox.localdomain>
2021-06-08 11:00:20 -0400

    Update vterm popup rule.

--- modules/term/personal-shell/config.el
+++ modules/term/personal-shell/config.el
@@ -1,7 +1,5 @@
 ;;; term/shell/config.el -*- lexical-binding: t; -*-
 
-(setq comint-get-old-input (lambda () "")
-      term-prompt-regexp "^.*@.*:.*\$")
+(setq comint-get-old-input (lambda () "") term-prompt-regexp "^.*@.*:.*\$")
 
-(set-popup-rule! "*doom:vterm.**" :quit nil :select t :width 120 :side 'right :ttl nil)
-t
+(set-popup-rule! "*doom:vterm.**" :quit nil :select t :width 0.38 :side 'right :ttl nil)
