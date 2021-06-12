4819d8f44e9214fd90b66ccd1850fc7a832f1a1f
Dodge Coates <dodge@goldbox.localdomain>
2021-06-12 12:24:40 -0400

    Install pygn correctly.

--- modules/app/chess/package.el
+++
@@ -1,3 +0,0 @@
-;;; app/chess/package.el -*- lexical-binding: t; -*-
-
-(package! pygn-mode :recipe (:host github :repo "dwcoates/pygn-mode"))
---
+++ modules/app/chess/packages.el
@@ -0,0 +1,4 @@
+;;; app/chess/package.el -*- lexical-binding: t; -*-
+
+(package! uci-mode :recipe (:type git :host github :repo "dwcoates/uci-mode"))
+(package! pygn-mode :recipe (:type git :host github :repo "dwcoates/pygn-mode"))
