d47ec746ad961814e95a238bbc7ad0d025521d15
Dodge Coates <dodge@goldbox.localdomain>
2021-06-13 07:58:07 -0400

    Support development recipe for pygn.

--- modules/app/chess/packages.el
+++ modules/app/chess/packages.el
@@ -1,4 +1,9 @@
 ;;; app/chess/package.el -*- lexical-binding: t; -*-
 
 (package! uci-mode :recipe (:type git :host github :repo "dwcoates/uci-mode"))
-(package! pygn-mode :recipe (:type git :host github :repo "dwcoates/pygn-mode"))
+
+;; If system defines a PYGNPATH, then we use that code for pygn, forgoing bytecompiling for
+;; development (no need to run `doom sync'). Otherwise, we install from official source.
+(if (and (getenv "PYGNPATH") (file-directory-p (getenv "PYGNPATH")))
+    (package! pygn-mode :recipe (:local-repo "~/workspace/pygn-mode" :build (:not compile)))
+ (package! pygn-mode :recipe (:type git :host github :repo "dwcoates/pygn-mode")))
