507d8aeac69c7f6ce3d3e8d044732a909ebdd18c
Dodge Coates <dodge@goldbox.localdomain>
2021-06-14 14:43:53 -0400

    Add CEE pygn-mode.

--- modules/app/chess/packages.el
+++ modules/app/chess/packages.el
@@ -1,10 +1,12 @@
 ;;; app/chess/package.el -*- lexical-binding: t; -*-
 
-(defmacro register-dwc-package (package-name package-local-env-name)
+(defmacro register-dwc-package (package-name package-local-env-name &optional only-local)
   "Define with `package!' a dwcoates repository.
 
 Prefer a local repository version denoted with system environment
-variable `PACKAGE-LOCAL-ENV-NAME' if available."
+variable `PACKAGE-LOCAL-ENV-NAME' if available.
+
+If `ONLY-LOCAL' is non-nil, do not look for package on github."
   (let ((package-local-path (getenv package-local-env-name)))
     (if package-local-path
       (progn
@@ -12,8 +14,10 @@ variable `PACKAGE-LOCAL-ENV-NAME' if available."
           (error "PYGNPATH environment variable points to a non-existent directory: '%s'"
                  package-local-path))
         `(package! ,package-name :recipe (:local-repo ,package-local-path :build (:not compile))))
-      `(package! ,package-name
-         :recipe (:type git :host github :repo ,(concat "dwcoates/" (symbol-name package-name)))))))
+      (unless ,only-local
+          `(package! ,package-name
+             :recipe (:type git :host github :repo ,(concat "dwcoates/" (symbol-name package-name))))))))
 
 (register-dwc-package pygn-mode "PYGNPATH")
 (register-dwc-package uci-mode "UCIMODEPATH")
+;; (register-dwc-package cee-pygn-mode "CEEPYGNPATH" t) ;; private code
