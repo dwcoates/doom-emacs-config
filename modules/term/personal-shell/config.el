4835f19e682f1d8c1ad16a44a9361a95f218eed8
Dodge Coates <dodge@goldbox.localdomain>
2021-06-20 09:53:28 -0400

    Make vterm popup a little bigger.

--- modules/term/personal-shell/config.el
+++ modules/term/personal-shell/config.el
@@ -2,4 +2,4 @@
 
 (setq comint-get-old-input (lambda () "") term-prompt-regexp "^.*@.*:.*\$")
 
-(set-popup-rule! "*doom:vterm.**" :quit nil :select t :width 0.38 :side 'right :ttl nil)
+(set-popup-rule! "*doom:vterm.**" :quit nil :select t :height 0.50 :side 'bottom :ttl nil)
