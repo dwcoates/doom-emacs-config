1b524a169f7c3d8b0f602a20b3e5e7c4d5a23d13
Dodge Coates <dodgecoates@silverbox.fios-router.home>
2024-07-26 19:12:11 -0400

    Log LSP interaction.

--- config.el
+++ config.el
@@ -90,6 +90,9 @@
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
+(after! lsp-mode
+  (setq lsp-log-io t))
+
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
 ;;
