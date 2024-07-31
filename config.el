a93fb9f4a4bc3bc96becd59be15a208028305943
Dodge Coates <dodgecoates@silverbox.fios-router.home>
2024-07-31 14:28:55 -0400

    Fix compilation scroll.

--- config.el
+++ config.el
@@ -54,18 +54,19 @@
 (set-popup-rule! "\\*helpful.*:.*\\*" :width 95 :quit t :side 'right)
 
 (setq
- ;;; From Henrik:
- ;;
- ;; "By default it should keep scrolling until it detects the first error. It's controlled by the
- ;;  compilation-scroll-output variable (set to 'first-error in doom, nil in vanilla. Set to t to
- ;;  always scroll). Also, it stops auto-scrolling if you scroll it manually, i believe."
- ;;
- ;; https://discordapp.com/channels/406534637242810369/406554085794381833/654479202560770065
- ;;
- ;;NOTE: For some reason, 'first-error isn't working for me. Set to t.
- compilation-scroll-output t
  fill-column 100)
 
+(after! compile
+  ;;; From Henrik:
+  ;;
+  ;; "By default it should keep scrolling until it detects the first error. It's controlled by the
+  ;;  compilation-scroll-output variable (set to 'first-error in doom, nil in vanilla. Set to t to
+  ;;  always scroll). Also, it stops auto-scrolling if you scroll it manually, i believe."
+  ;;
+  ;; https://discordapp.com/channels/406534637242810369/406554085794381833/654479202560770065
+  ;;
+  ;;NOTE: For some reason, 'first-error isn't working for me. Set to t.
+  (setq compilation-scroll-output t))
 ;; Some UI doodads
 (display-time)
 (set-face-attribute 'default nil :height 125)
