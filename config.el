4b14f42a34fb4e2fbb9e3e1d42e9a2624c5e69b3
Dodge Coates <dodgecoates@silverbox.fios-router.home>
2024-08-02 15:02:58 -0400

    Don't quit helpful.

--- config.el
+++ config.el
@@ -51,7 +51,7 @@
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type 'relative)
 
-(set-popup-rule! "\\*helpful.*:.*\\*" :width 95 :quit t :side 'right)
+(set-popup-rule! "\\*helpful.*:.*\\*" :width 95 :quit nil :side 'right)
 
 (setq
  fill-column 100)
