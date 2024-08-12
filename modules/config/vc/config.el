1ad7437e5b57734b5e2744a09c722551f8dad9e2
Dodge Coates <dodgecoates@silverbox.fios-router.home>
2024-08-12 19:34:43 -0400

    Turn off blamer, it sucks.

--- modules/config/vc/config.el
+++ modules/config/vc/config.el
@@ -7,16 +7,16 @@
         git-messenger:use-magit-popup t))
 
 ;; (after! personal-theme ;;FIXME: this doesn't work, because personal-theme is not being properly provided as a package
-(use-package! blamer
-  :bind ("s-i" . blamer-show-commit-info)
-  :defer 5
-  :custom
-  (blamer-idle-time 0.3)
-  (blamer-min-offset 20)
-  :custom-face
-  (blamer-face ((t :foreground ,(face-attribute 'font-lock-comment-face :foreground)
-                   :background nil
-                   :italic t)))
-  :config
-  (global-blamer-mode 1))
-;; )
+;;   (use-package! blamer
+;;     :bind ("s-i" . blamer-show-commit-info)
+;;     :defer 5
+;;     :custom
+;;     (blamer-idle-time 0.3)
+;;     (blamer-min-offset 20)
+;;     :custom-face
+;;     (blamer-face ((t :foreground ,(face-attribute 'font-lock-comment-face :foreground)
+;;                      :background nil
+;;                      :italic t)))
+;;     :config
+;;     (global-blamer-mode 1))
+;;   )
