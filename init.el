33bdc622f6bfb0c83c1e6aef1279451412510ac6
Dodge Coates <dodgecoates@silverbox.fios-router.home>
2024-08-12 19:36:49 -0400

    Disable Python LSP.

--- init.el
+++ init.el
@@ -160,7 +160,7 @@
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
        protobuf
-       (python +lsp +tree-sitter)            ; beautiful is better than ugly
+       (python +tree-sitter)            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
