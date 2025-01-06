b2a6a0b5788a0b321392a82e31c33aaa2b1afb25
Dodge W. Coates <dodge.w.coates@gmail.com>
2025-01-06 13:06:13 -0500

    Fixup lsp/treesitter for some languages.

--- init.el
+++ init.el
@@ -160,7 +160,7 @@
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
        protobuf
-       (python +tree-sitter)            ; beautiful is better than ugly
+       (python +tree-sitter +lsp)            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
@@ -170,10 +170,10 @@
        ;;(rust +lsp)       ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;(scheme +guile)   ; a fully conniving family of lisps
-       (sh +lsp tree-sitter)                ; she sells {ba,z,fi}sh shells on the C xor
+       (sh +lsp +tree-sitter)                ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml
        ;;solidity          ; do you need a blockchain? No.
-       ;;swift             ; who asked for emoji variables?
+       (swift +lsp +tree-sitter)             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
        ;;web               ; the tubes
        (yaml +tree-sitter)              ; JSON, but readable
